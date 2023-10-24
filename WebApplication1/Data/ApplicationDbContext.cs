7b1b4f61183237aacfd5f2a782fcd2fd66ed5ce1
Grégory <gregory.schoemaecker@outlook.fr>
2023-10-24 09:23:07 +0200

    MIse en place du Data Seed

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -26,14 +26,15 @@ namespace WebApplication1.Data
                 new Specy { Id = 4, Name = "Brachiosaurus" },
                 new Specy { Id = 5, Name = "Stegosaurus" },
                 new Specy { Id = 7 ,Name = "Ankylosaures " }, /*cuirassé*/
-                new Specy { Id = 8, Name = "Hadrosaures " } /*bec de canard*/
+                new Specy { Id = 8, Name = "Hadrosaures " }, /*bec de canard*/
+                 new Specy { Id = 9, Name = "Ptéranodon" }
                 );
             builder.Entity<Dinosaur>().HasData(
                 new Dinosaur
                 {
                     Id = 1,
                     FoodId = 1,
-                    SpacyId = 1,
+                    SpecyId = 1,
                     ImagePath = "https://imagenes.razon.com.mx/files/image_940_470/files/fp/uploads/2022/11/05/63673d376c8d7.r_d.574-112-0.jpeg",
                     Name = "Blue",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
@@ -49,9 +50,9 @@ namespace WebApplication1.Data
                  new Dinosaur
                  {
                      Id = 2,
-                     FoodId = 2,
-                     ImagePath = "https://imagenes.razon.com.mx/files/image_940_470/files/fp/uploads/2022/11/05/63673d376c8d7.r_d.574-112-0.jpeg",
-                     Name = "Green",
+                     FoodId = 1,
+                     ImagePath = "https://i.pinimg.com/1200x/3f/09/89/3f0989c1631958ae460ec8e28976d3d4.jpg",
+                     Name = "Jean",
                      Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
                     "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
                     "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
@@ -61,7 +62,177 @@ namespace WebApplication1.Data
                     " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
                     "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
                     " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                 }                
+                 },
+                    new Dinosaur
+                    {
+                        Id = 3,
+                        FoodId = 1,
+                        SpecyId = 1,
+                        ImagePath = "https://www.sideshow.com/storage/product-images/910418/baby-blue_jurassic-world-fallen-kingdom_square.jpg",
+                        Name = "Claude",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                    " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    },
+                    new Dinosaur
+                    {
+                        Id = 4,
+                        FoodId = 1,
+                        SpecyId = 1,
+                        ImagePath = "https://www.tomsguide.fr/content/uploads/sites/2/2020/06/jurassic-park.jpg",
+                        Name = "Charlie",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                    " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                           },
+                    new Dinosaur
+                    {
+                        Id = 5,
+                        FoodId = 1,
+                        SpecyId = 2,
+                        ImagePath = " https://static.wikia.nocookie.net/jurassicpark/images/3/32/Tyrannosaure_m%C3%A2le.png/revision/latest?cb=20190702163733&path-prefix=fr",
+                        Name = "Rex",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                    " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                     },
+                    new Dinosaur
+                    {
+                        Id = 6,
+                        FoodId = 2,
+                        SpecyId = 3,
+                        ImagePath = "https://cdn.mos.cms.futurecdn.net/eFkkKndqdwU8n9qfTzP9CU.jpg",
+                        Name = "Jacqueline",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                    " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                                     },
+                    new Dinosaur
+                    {
+                        Id = 7,
+                        FoodId = 2,
+                        SpecyId = 5,
+                        ImagePath = "https://www.jurassic-park.fr/fckeditor_upload/image/the-lost-world/Dinosaures/St%C3%A9go%20TLW.jpg",
+                        Name = "Jacqueline",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                    " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                                           },
+                    new Dinosaur
+                    {
+                        Id = 8,
+                        FoodId = 2,
+                        SpecyId = 5,
+                        ImagePath = "https://thumb.canalplus.pro/http/unsafe/1440x810/filters:quality(80)/img-hapi.canalplus.pro:80/ServiceImage/ImageID/105995798",
+                        Name = "stego-junior",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                    " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                     },
+                      new Dinosaur
+                      {
+                          Id = 9,
+                          FoodId = 2,
+                          SpecyId = 4,
+                          ImagePath = "https://www.jurassic-park.fr/fckeditor_upload/image/jurassic-park/dinosaures/nouvelle%20version/Brach%20JP.png",
+                          Name = "Grand Pied",
+                          Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                    " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                      },
+                    new Dinosaur
+                    {
+                        Id = 10,
+                        FoodId = 2,
+                        SpecyId = 4,
+                        ImagePath = "https://i0.wp.com/the-avocado.org/wp-content/uploads/2018/06/Jurassic-World-Fallen-Kingdom-1.jpeg?fit=960%2C539&ssl=1",
+                        Name = "Alexa",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                    " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    },
+                          new Dinosaur
+                          {
+                              Id = 11,
+                              FoodId = 1,
+                              SpecyId = 2,
+                              ImagePath = "https://i0.wp.com/the-avocado.org/wp-content/uploads/2018/06/Jurassic-World-Fallen-Kingdom-1.jpeg?fit=960%2C539&ssl=1",
+                              Name = "Rexa",
+                              Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                    " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                          },
+                    new Dinosaur
+                    {
+                        Id = 12,
+                        FoodId = 1,
+                        SpecyId = 9,
+                        ImagePath = "https://www.jurassicworlduniverse.com/wordpress/wp-content/uploads/2014/11/pteranodon04.jpg",
+                        Name = "Marceau",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                    " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    }
                 );
         }
     }
